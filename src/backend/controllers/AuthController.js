7563cf6acece7a05cebb63a9b49d53951fc09090
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-03-14 14:44:16 +0530

    [chore] router dependency, favicon

--- src/backend/controllers/AuthController.js
+++ src/backend/controllers/AuthController.js
@@ -1,4 +1,4 @@
-import { v4 as uuid } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import { Response } from "miragejs";
 import { formatDate } from "../utils/authUtils";
 import bcrypt from "bcryptjs";
@@ -29,7 +29,7 @@ export const signupHandler = function (schema, request) {
         }
       );
     }
-    const _id = uuid();
+    const _id = uuidv4();
     const encryptedPassword = bcrypt.hashSync(password, 5);
     const newUser = {
       _id,
