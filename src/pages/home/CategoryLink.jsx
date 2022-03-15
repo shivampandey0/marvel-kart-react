a5a4e3a8badf6c8116ba410d1279f3a6edd66c33
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-03-15 20:44:09 +0530

    [feat] Featured Category Component

---
+++ src/pages/home/CategoryLink.jsx
@@ -0,0 +1,13 @@
+import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
+
+export const CategoryLink = ({ categoryImg, categoryName }) => {
+  return (
+    <>
+      <Link to="/products" className="flex-column flex-center">
+        <img  src={categoryImg} alt="" className={styles.home_featured} />
+        <div className={styles.category_name}>{categoryName}</div>
+      </Link>
+    </>
+  );
+};
