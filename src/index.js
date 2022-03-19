c11303b217b89ec57cc63c189f73a5a2efef5a5b
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-03-19 20:13:16 +0530

    [chore][fix] imports fix

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom";
 import "./css/index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { BrowserRouter } from "react-router-dom";
+import { DataProvider } from "./context";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <DataProvider>
+        <App />
+      </DataProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
