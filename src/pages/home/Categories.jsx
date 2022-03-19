3fe652917307c23acad1f6cc978a528cc429a3d2
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-03-19 20:02:48 +0530

    [refactor] categories axios refactor

--- src/pages/home/Categories.jsx
+++ src/pages/home/Categories.jsx
@@ -1,31 +1,36 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { CategoryLink } from "./CategoryLink";
-import axios from "axios";
+import { useAxios } from "../../hooks";
+import { Loader } from "../../components";
+import { useData } from "../../context";
+import { ACTION_TYPE } from "../../utils";
 
 export const Categories = () => {
-  const [categories, setCategories] = useState([]);
+  const { response, error, loading } = useAxios({
+    method: "get",
+    url: "categories",
+  });
+
+  const { state, dispatch } = useData();
 
   useEffect(() => {
-    (async () => {
-      try {
-        const res = await axios.get("api/categories");
-        setCategories(res.data.categories);
-      } catch (error) {
-        console.log(error);
-        throw error;
-      }
-    })();
-  }, []);
+    if (response) {
+      dispatch({ type: ACTION_TYPE.CATEGORIES, payload: response.categories });
+    }
+  }, [response]);
 
   return (
     <>
-      {categories.map(({ _id, categoryImage, categoryName }) => (
-        <CategoryLink
-          key={_id}
-          categoryImg={categoryImage}
-          categoryName={categoryName}
-        />
-      ))}
+      {state.categories &&
+        state.categories.map(({ _id, categoryImage, categoryName }) => (
+          <CategoryLink
+            key={_id}
+            categoryImg={categoryImage}
+            categoryName={categoryName}
+          />
+        ))}
+
+      {loading && <Loader />}
     </>
   );
 };
