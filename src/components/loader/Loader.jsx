58d693dd3b41cf66f850939eb11416091ba25441
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-03-19 20:01:34 +0530

    [refactor] Loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -2,7 +2,7 @@ import "../../css/loader.css";
 
 export const Loader = () => {
   return (
-    <div className="pos-fixed flex-row justify-cntr aign-cntr">
+    <div className="center-loader">
       <div className="loader"></div>
     </div>
   );
