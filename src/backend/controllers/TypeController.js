3cf4c1a010b8de2e9946cf97d5514ad3572ac465
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-03-20 22:49:25 +0530

    [feat] Product Type endpoint in backend

---
+++ src/backend/controllers/TypeController.js
@@ -0,0 +1,48 @@
+import { Response } from "miragejs";
+
+/**
+ * All the routes related to Product Type are present here.
+ * These are Publicly accessible routes.
+ * */
+
+/**
+ * This handler handles gets all product types in the db.
+ * send GET Request at /api/types
+ * */
+
+export const getAllTypesHandler = function () {
+  console.log(this.db.types);
+  console.log(this.db.categories);
+  try {
+    return new Response(200, {}, { types: this.db.types });
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
+
+/**
+ * This handler handles gets all product types in the db.
+ * send GET Request at /api/user/type/:TypeId
+ * */
+
+export const getTypeHandler = function (schema, request) {
+  const TypeId = request.params.TypeId;
+  try {
+    const type = schema.types.findBy({ _id: TypeId });
+    return new Response(200, {}, { type });
+  } catch (error) {
+    new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
