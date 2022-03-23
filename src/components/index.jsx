bcc48cc8f181c0e78357774a17e9f89104591047
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-03-24 01:34:35 +0530

    [feat] [style] Form Error component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,6 +3,7 @@ export { Header } from "./header/Header";
 export { Footer } from "./footer/Footer";
 export { Loader } from "./loader/Loader";
 export { Input } from "./input/Input";
+export { FormError } from "./formError/FormError";
 export { Fieldset } from "./fieldset/Fieldset";
 export { AuthLeftContainer } from "./AuthLeftContainer";
 export { FilterInput } from "./filterInput/FilterInput";
