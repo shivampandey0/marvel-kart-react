e6cb91a0715222ba401ebb3cc86c5c2e5ca91988
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-03-24 19:05:52 +0530

    [feat] [WIP] AuthContext and Provider

---
+++ src/context/authContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+const useAuth = () => useContext(AuthContext);
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({ token: localStorage.getItem("token") });
+
+  const logoutUser = () => {
+    setAuth();
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth, logoutUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { useAuth, AuthProvider };
