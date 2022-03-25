7bb2d79c7de8861a9ff50dad8824082c364211d6
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-03-25 11:28:15 +0530

    [chore] empty wishlist page component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 export { Home } from "./home/Home";
 export { Register } from "./register/Register";
 export { Login } from "./login/Login";
+export { Wishlist } from "./wishlist/Wishlist";
 export { Products } from "./products/Products";
---
+++ src/pages/wishlist/Wishlist.jsx
@@ -0,0 +1,7 @@
+
+export const Wishlist = () => {
+  return (
+    <div>Wishlist</div>
+  )
+}
+
