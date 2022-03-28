f754780f0b825b86be71298b36544abfee323ff7
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-03-28 21:20:28 +0530

    [feat] cart page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 export { Home } from "./home/Home";
 export { Register } from "./register/Register";
 export { Login } from "./login/Login";
+export { Cart } from "./cart/Cart";
 export { Wishlist } from "./wishlist/Wishlist";
 export { Products } from "./products/Products";
