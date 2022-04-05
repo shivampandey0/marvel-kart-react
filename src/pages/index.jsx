ff367890783d9c4731f3ca90d5af2a732f913ae1
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-04-05 18:53:43 +0530

    [feat] Error route page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,5 +2,6 @@ export { Home } from "./home/Home";
 export { Register } from "./register/Register";
 export { Login } from "./login/Login";
 export { Cart } from "./cart/Cart";
+export { Error } from "./error/Error";
 export { Wishlist } from "./wishlist/Wishlist";
 export { Products } from "./products/Products";
