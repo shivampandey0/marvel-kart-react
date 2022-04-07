be7f564c0f1f61a308cf306d564496b78bba1feb
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-04-07 13:20:58 +0530

    [refactor] Empty data message

--- src/pages/products/ProductsGrid.jsx
+++ src/pages/products/ProductsGrid.jsx
@@ -7,12 +7,16 @@ export const ProductsGrid = ({ loading }) => {
   return (
     <>
       <section className="listings-products">
-        <div className="products-wrapper">
-          {filteredData &&
-            filteredData.map((product) => (
-              <Product key={product._id} product={product} />
-            ))}
-        </div>
+        {filteredData.length === 0 ? (
+          <div className="txt-center my-8">No Products Found</div>
+        ) : (
+          <div className="products-wrapper">
+            {filteredData &&
+              filteredData.map((product) => (
+                <Product key={product._id} product={product} />
+              ))}
+          </div>
+        )}
         {loading && <Loader />}
       </section>
     </>
--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -7,12 +7,16 @@ export const Wishlist = () => {
   return (
     <main className="container ">
       <h3 className="txt-center fw-bold my-8">My Wishlist</h3>
-      <section className="products-wrapper">
-        {wishlist &&
-          wishlist.map((product) => (
-            <WishCard key={product._id} product={product} />
-          ))}
-      </section>
+      {wishlist.length ? (
+        <section className="products-wrapper">
+          {wishlist &&
+            wishlist.map((product) => (
+              <WishCard key={product._id} product={product} />
+            ))}
+        </section>
+      ) : (
+        <h3 className="txt-center fw-bold">Wishlist is Empty</h3>
+      )}
     </main>
   );
 };
